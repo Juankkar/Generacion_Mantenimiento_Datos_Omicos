2bb91aa14cc6cdddc6a51433067b45ae67dd41b6
Juan Carlos <jcge97@gmail.com>
2023-01-14 22:45:10 +0000

    Terminada la actividad1, modificación de .gitignore para no enviar los fastq al repositorio

--- actividad1/code/leer_fastq.sh
+++ actividad1/code/leer_fastq.sh
@@ -9,13 +9,14 @@ read -p "Seleccione la lista con sus runs -> " runs
 ## Cargamos otra variable con el archivo abierto de la variable anterior
 sra_list=$(cat $runs)
 
-
 ###############
 ## EJECUCIÓN ##
 ###############
 
 ## Creamos un bucle que descargue de forma automática los archivos
-# for sra in $sra_list; do
-#  fastq-dump --split-files $sra
-# done
+for sra in $sra_list
+do
+    fastq-dump --split-files $sra
+done
 
+mv *.fastq ../data
